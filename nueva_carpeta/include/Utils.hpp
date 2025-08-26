f14351d1cd9bbf3c4f571c1135b4c44f5ad120a7
STroglia <111158001+STroglia@users.noreply.github.com>
2025-08-26 19:34:43 -0300

    Delete nueva_carpeta directory

--- nueva_carpeta/include/Utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace Utils
-{
-    std::string getFileNameFromPath(const std::string& filepath);
-    std::string getDirectoryFromPath(const std::string& filePath);
-}
